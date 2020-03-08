56b31c91da7ee15ee8f91a8d0e2b8e6b365ae13e
Roman <romanzhydyk41@gmail.com>
2020-03-08 17:06:25 +0200

    Create login page and create User interface

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,6 +6,7 @@ import { RouterModule } from "@angular/router";
 import { DashboardPageComponent } from "./dashboard-page/dashboard-page.component";
 import { CreatePageComponent } from "./create-page/create-page.component";
 import { EditPageComponent } from "./edit-page/edit-page.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -17,6 +18,8 @@ import { EditPageComponent } from "./edit-page/edit-page.component";
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       {
         path: "",
